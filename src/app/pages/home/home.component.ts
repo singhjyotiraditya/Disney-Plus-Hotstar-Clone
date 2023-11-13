5e6beaca4ed034b1adb2b42031a3499d51321da3
Jyotiraditya Singh <adsi123qwe@gmail.com>
2023-11-13 17:08:52 +0530

    initial commit

---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,112 @@
+import { Component, OnInit } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
+import { Router } from '@angular/router';
+import { Movie } from 'src/app/interfaces/movies';
+import { MoviesService } from 'src/app/services/movies.service';
+import { UserService } from 'src/app/services/user.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+
+  trendingMoviesResults?: Movie[] = [];
+  discoverMoviesResults?: Movie[] = [];
+  actionMovieResults?: Movie[] = [];
+  adventureMovieResults?: Movie[] = [];
+  animationMovieResults?: Movie[] = [];
+  comedyMovieResults?: Movie[] = [];
+  documentaryMovieResults?: Movie[] = [];
+  sciencefictionMovieResults?: Movie[] = [];
+  thrillerMovieResults?: Movie[] = [];
+
+  constructor (private moviesService: MoviesService, private userService: UserService, private title: Title, private meta: Meta, private router: Router) { }
+
+  ngOnInit (): void {
+    this.trendingMovies();
+    this.discoverMovies();
+    this.actionMovies();
+    this.adventureMovies();
+    this.comedyMovies();
+    this.animationMovies();
+    this.documentaryMovies();
+    this.sciencefictionMovies();
+    this.thrillerMovies();
+  }
+
+
+  onWheel(event: WheelEvent): void {
+    const amount = 500; 
+
+    event.preventDefault();
+
+    const direction = event.deltaY || (event as any).wheelDelta;
+    const element = document.getElementById('box') as HTMLElement;
+
+    if (element) {
+      const position = element.scrollLeft;
+      element.scrollLeft = position + (direction > 0 ? -amount : amount);
+    }
+  }
+
+
+
+
+  trendingMovies () {
+    this.moviesService.getTrendingMovies().subscribe((result) => {
+      console.log(result, 'trendingresult#');
+      this.trendingMoviesResults = result.results;
+    });
+  }
+
+  discoverMovies () {
+    this.moviesService.getDiscoverMovies().subscribe((result) => {
+      console.log(result, 'discoverresult#');
+      this.discoverMoviesResults = result.results;
+    });
+  }
+
+  actionMovies () {
+    this.moviesService.getActionMovies().subscribe((result) => {
+      this.actionMovieResults = result.results;
+    });
+  }
+
+  adventureMovies () {
+    this.moviesService.getAdventureMovies().subscribe((result) => {
+      this.adventureMovieResults = result.results;
+    });
+  }
+
+  animationMovies () {
+    this.moviesService.getAnimationMovies().subscribe((result) => {
+      this.animationMovieResults = result.results;
+    });
+  }
+
+  comedyMovies () {
+    this.moviesService.getComedyMovies().subscribe((result) => {
+      this.comedyMovieResults = result.results;
+    });
+  }
+
+  documentaryMovies () {
+    this.moviesService.getDocumentaries().subscribe((result) => {
+      this.documentaryMovieResults = result.results;
+    });
+  }
+
+  sciencefictionMovies () {
+    this.moviesService.getScienceFictionMovies().subscribe((result) => {
+      this.sciencefictionMovieResults = result.results;
+    });
+  }
+
+  thrillerMovies () {
+    this.moviesService.getThrillerMovies().subscribe((result) => {
+      this.thrillerMovieResults = result.results;
+    });
+  }
+}
---
+++ src/app/pages/movies-slider/movies-slider.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { MoviesService } from 'src/app/services/movies.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-movies-slider',
+  templateUrl: './movies-slider.component.html',
+  styleUrls: ['./movies-slider.component.scss']
+})
+export class MoviesSliderComponent implements OnInit {
+
+  bannerResult?: any = [];
+
+  constructor (private moviesService: MoviesService) { }
+
+  ngOnInit (): void {
+    this.bannerData();
+  }
+
+  bannerData () {
+    this.moviesService.sliderMovies().subscribe((result) => {
+      this.bannerResult = result.results;
+    });
+  }
+
+}
